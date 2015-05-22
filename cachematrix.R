5f5ca8489d8248ee84f776a6519c43cc58ecb177
Rahul Joshi <rahuldjoshi83@gmail.com>
2015-05-22 17:35:04 -0500

    Commiting code for cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function 
+## This functions accepts matrix as an argument with an assumption,
+## it's reversible and returns the list of setters and getters functions
 
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y){
+                x <<- y    
+                ## to comply with '(and the matrix has not changed)' requirement
+                i <<- NULL             
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get, 
+             setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This functions accepts the matrix created by makeCacheMatrix 
+## function as an argument, checks if inverse is already
+## calculated for given matrix x (and if it's not changed) and if
+## so, returns the cached result. Else, it will calculate the inverse
+## of a matrix and cache the result for future re-use.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {       
+        i <- x$getinverse()
+        if(!is.null(i)){
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
